4034242aee5acce9579b91886482b0f812105cbe
Noushin Anika Khan <noushinanikakhan@gmail.com>
2025-11-07 19:39:00 +0600

    first

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require ('cors')
+const app = express()
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+const uri = "mongodb+srv://SmartDbUser:YfsVErx8Q5F6vDZ3@cluster0.a0a09os.mongodb.net/?appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Smart server is running!')
+})
+
+async function run (){
+     try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+     const db = client.db('smart_db');
+     const productsCollection = db.collection ('products')
+
+    app.get('/products', async (req, res) => {
+       const cursor = productsCollection.find();
+       const result = await cursor.toArray()
+        res.send(result)
+     })
+
+     app.get('/products/:id', async (req, res)=>{
+            const id = req.params.id;
+             const query = { _id: new ObjectId(id)}
+             const result = await productsCollection.findOne(query)
+              res.send(result)
+
+     })
+
+     app.post('/products', async (req, res) => {
+        const newProduct = req.body;
+        const result = await productsCollection.insertOne(newProduct)
+        res.send(result)
+     })
+
+     app.patch('/products/:id', async (req, res)=> {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            const query = { _id: new ObjectId(id)}
+            const update = {
+                $set: {
+                name: updatedProduct.name,
+                price: updatedProduct.price
+                }
+            }
+       const result = await productsCollection.updateOne(query, update) 
+       res.send(result)
+
+     })
+
+
+     app.delete('/products/:id', async (req, res)=> {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)}
+            const result= await productsCollection.deleteOne(query)
+        res.send(result);
+     })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+  } finally {
+   
+  }
+
+}
+
+run().catch(console.dir)
+
+app.listen(port, () => {
+  console.log(`Smart server is running on port ${port}`)
+})
